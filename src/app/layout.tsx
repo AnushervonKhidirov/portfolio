5340438c85ce10679cf17e1c0689f8e036abcfb8
AnushervonKhidirov <ankhid98@gmail.com>
2024-10-07 20:00:36 +0500

    local fonts added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,43 @@
 import type { Metadata } from 'next'
 
-import { Oswald } from 'next/font/google'
+import localFonts from 'next/font/local'
 import Header from '@component/header/header'
 
 import './globals.css'
 
-const font = Oswald({
-    weight: ['200', '300', '400', '500', '600', '700'],
-    style: 'normal',
-    subsets: ['latin'],
+const font = localFonts({
+    src: [
+        {
+            path: '../../public/assets/fonts/Oswald/Oswald-ExtraLight.woff2',
+            weight: '200',
+            style: 'normal',
+        },
+        {
+            path: '../../public/assets/fonts/Oswald/Oswald-Light.woff2',
+            weight: '200',
+            style: 'normal',
+        },
+        {
+            path: '../../public/assets/fonts/Oswald/Oswald-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../../public/assets/fonts/Oswald/Oswald-Medium.woff2',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: '../../public/assets/fonts/Oswald/Oswald-SemiBold.woff2',
+            weight: '600',
+            style: 'normal',
+        },
+        {
+            path: '../../public/assets/fonts/Oswald/Oswald-Bold.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+    ],
 })
 
 export const metadata: Metadata = {
