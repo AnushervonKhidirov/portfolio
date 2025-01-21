1141daeeff9b6fbc18813f04ba5a4bf31b4fbe9a
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-21 22:06:38 +0500

    introduction updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 import Scroller from '@common/scroller/scroller'
 import IntroductionSection from '@component/sections/introduction/introduction-section'
-import OrganizationSection from '@component/sections/organization/organization-section'
-import SkillsSection from '@component/sections/skills/skills-section'
-import ProjectSection from '@component/sections/projects/project-section'
-import ContactsSection from '@component/sections/contacts/contacts-section'
+// import OrganizationSection from '@component/sections/organization/organization-section'
+// import SkillsSection from '@component/sections/skills/skills-section'
+// import ProjectSection from '@component/sections/projects/project-section'
+// import ContactsSection from '@component/sections/contacts/contacts-section'
 
-import { introduction, experiences, trainings, skillList, projects, contacts } from '@constant/data'
+// import { experiences, trainings, skillList, projects, contacts } from '@constant/data'
 
 const Page = () => {
     return (
         <Scroller pageScroller handleMenu>
-            <IntroductionSection data={introduction} />
-            <OrganizationSection title="Experience" data={experiences} />
-            <OrganizationSection title="Trainings" data={trainings} />
-            <SkillsSection data={skillList} />
-            <ProjectSection data={projects} />
-            <ContactsSection data={contacts} />
+            <IntroductionSection />
+            {/* <OrganizationSection title="Experience" data={experiences} /> */}
+            {/* <OrganizationSection title="Trainings" data={trainings} /> */}
+            {/* <SkillsSection data={skillList} /> */}
+            {/* <ProjectSection data={projects} /> */}
+            {/* <ContactsSection data={contacts} /> */}
         </Scroller>
     )
 }
