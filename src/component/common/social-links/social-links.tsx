eba6dd8475978eb2dd0534c818ed810603cc13d1
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-21 22:05:37 +0500

    social links component updated

--- src/component/common/social-links/social-links.tsx
+++ src/component/common/social-links/social-links.tsx
@@ -1,23 +1,37 @@
 import type { FC } from 'react'
 import type { AdditionalProps } from '@type/common'
-import type { TSocialLinkList } from '@type/social-links'
+import type { TSocialLinksProps } from '@type/social-links.type'
 
 import Link from 'next/link'
 import Tooltip from '@common/tooltip/tooltip'
 
+import { GitHubIcon, LinkedInIcon, LeetCodeIcon, RoadmapIcon, WebsiteIcon } from '@component/svg'
+
 import classNames from 'classnames'
 import classes from './social-links.module.css'
 
-const SocialLinks: FC<AdditionalProps<TSocialLinkList>> = ({ socialLinks, className }) => {
+const SocialLinks: FC<AdditionalProps<TSocialLinksProps>> = ({ socialLinks, className }) => {
+    const Icons: { [key: string]: () => JSX.Element } = {
+        GitHub: GitHubIcon,
+        LinkedIn: LinkedInIcon,
+        LeetCode: LeetCodeIcon,
+        Roadmap: RoadmapIcon,
+        Website: WebsiteIcon,
+    }
+
     return (
         <div className={classNames(classes.social_links, className)}>
-            {socialLinks.map(({ title, href, Icon }) => (
-                <Tooltip title={title} key={href}>
-                    <Link href={href} target="_blank" className={classes.social_link}>
-                        <Icon />
-                    </Link>
-                </Tooltip>
-            ))}
+            {socialLinks.map(({ id, title, href }) => {
+                const Icon = Icons[title]
+
+                return (
+                    <Tooltip title={title} key={id}>
+                        <Link href={href} target="_blank" className={classes.social_link}>
+                            <Icon />
+                        </Link>
+                    </Tooltip>
+                )
+            })}
         </div>
     )
 }
