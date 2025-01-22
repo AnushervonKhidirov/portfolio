be28282519270d7f2671216719c448cd37973b11
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-22 11:20:44 +0500

    introduction section updated

--- src/component/sections/introduction/introduction-section.tsx
+++ src/component/sections/introduction/introduction-section.tsx
@@ -5,8 +5,6 @@ import type { TSocialLink } from '@type/social-links.type'
 import { useEffect, useState } from 'react'
 
 import { User } from '@services/user'
-import { Positions } from '@services/positions'
-import { Grades } from '@services/grades'
 import { SocialLinks } from '@services/social-links'
 
 import Section from '@common/section/section'
@@ -17,34 +15,22 @@ import classes from './introduction-section.module.css'
 
 const IntroductionSection = () => {
     const user = new User()
-    const positions = new Positions()
-    const grades = new Grades()
     const socialLinks = new SocialLinks()
 
     const [userInfo, setUserInfo] = useState<TUser | null>(null)
-    const [position, setPosition] = useState<string>('')
-    const [grade, setGrade] = useState<string>('')
     const [socialLinksList, setSocialLinksList] = useState<TSocialLink[]>([])
 
-    async function collectData() {
+    async function getUserInfo() {
         const userInfo = await user.getUserInfo()
         if (!userInfo) return
         setUserInfo(userInfo)
-
-        const position = await positions.getPosition(userInfo.positionId)
-        if (!position) return
-        setPosition(position.value)
-
-        const grade = userInfo.gradeId ? await grades.getGrade(userInfo.gradeId) : null
-        if (!grade) return
-        setGrade(grade.value)
 
         const socialLinkList = await socialLinks.getSocialLinks()
         setSocialLinksList(socialLinkList)
     }
 
     useEffect(() => {
-        collectData()
+        getUserInfo()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
@@ -55,8 +41,8 @@ const IntroductionSection = () => {
                     className={classes.introduction}
                     name={userInfo.name}
                     about={userInfo.about}
-                    position={position}
-                    grade={grade}
+                    position={userInfo.position}
+                    grade={userInfo.grade}
                     socialLinks={socialLinksList}
                 />
                 <LaptopImage className={classes.image} />
